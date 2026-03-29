56e2238ed6d19d9c0a66133700286b35d93cdc09
SajibSaha207 <sajibsaha.207@gmail.com>
2026-03-29 22:26:59 +0600

    added firebase auth

---
+++ src/Context/AuthProvider.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AuthContext } from './AuthContext';
+
+const AuthProvider = ({children}) => {
+
+const authinfo = {
+
+}
+
+    return (
+        <AuthContext value={authinfo}>
+            {children}
+        </AuthContext>
+    );
+};
+
+export default AuthProvider;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router';
+import RootLayout from './Layout/RootLayout.jsx';
+import Home from './Components/Home/Home.jsx';
+import AllProducts from './Components/AllProducts/AllProducts.jsx';
+import AuthProvider from './Context/AuthProvider.jsx';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    Component:RootLayout,
+  children:[
+    {
+      index: true,
+      Component:Home
+    },
+    {
+      path:"allProducts",
+      Component: AllProducts
+    }
+  ]
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>,
+)
