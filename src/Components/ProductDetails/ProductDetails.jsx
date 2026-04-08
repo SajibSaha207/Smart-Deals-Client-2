a3a11644c4ffb1928996a62a0aaf55791fccc1aa
SajibSaha207 <sajibsaha.207@gmail.com>
2026-04-08 11:00:29 +0600

    added bidmodal

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useLoaderData } from 'react-router';
 
 const ProductDetails = () => {
     const product = useLoaderData();
+    const bidmodalRef = useRef(null);
     console.log(product)
+
+    const handleBidModalOpen = () =>{
+        bidmodalRef.current.showModal();
+    }
     return (
         <div>
             {/* product info */}
@@ -12,7 +17,19 @@ const ProductDetails = () => {
 
                 </div>
                 <div>
-                    <button className='btn btn-primary'>I want to buy this product</button>
+                    <button onClick={handleBidModalOpen} className='btn btn-primary'>I want to buy this product</button>
+<dialog ref={bidmodalRef} className="modal modal-bottom sm:modal-middle">
+  <div className="modal-box">
+    <h3 className="font-bold text-lg">Hello!</h3>
+    <p className="py-4">Press ESC key or click the button below to close</p>
+    <div className="modal-action">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
                 </div>
             </div>
             {/* bids for this product */}
